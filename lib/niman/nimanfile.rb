9d173698e795dbabcc4057b2d6d217a0f36e79a2
Jan Schulte <hello@unexpected-co.de>
2015-02-03 22:59:31 +0100

    file method calls block with config object

--- lib/niman/nimanfile.rb
+++ lib/niman/nimanfile.rb
@@ -1,6 +1,9 @@
+require 'niman/library/file'
+
 module Niman
   class Nimanfile
     def file
+      yield(Niman::Library::File.new)
     end
   end
 end
--- spec/lib/nimanfile_spec.rb
+++ spec/lib/nimanfile_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 require 'niman/nimanfile'
+require 'niman/library/file'
 
 describe Niman::Nimanfile do
   subject(:niman_file) { Niman::Nimanfile.new }
 
   it 'has file method' do
     expect(niman_file.respond_to?(:file)).to be true
+  end
+
+  specify 'file calls block with config object' do
+    expect { |b| niman_file.file(&b) }.to yield_with_args(Niman::Library::File)
   end
 end
