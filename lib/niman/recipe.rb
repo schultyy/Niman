95784a02913443380d84dbff59952fec1cd50247
Jan Schulte <hello@unexpected-co.de>
2015-02-07 00:14:29 +0100

    can load configuration from file

--- lib/niman/recipe.rb
+++ lib/niman/recipe.rb
@@ -2,6 +2,7 @@ require 'niman/nimanfile'
 
 module Niman
   class Recipe
+    DEFAULT_FILENAME = 'Nimanfile'
     class << self
       attr_writer :configuration
     end
@@ -17,6 +18,11 @@ module Niman
 
     def self.reset!
       @configuration = Niman::Nimanfile.new
+    end
+
+    def self.from_file
+      path = File.join(Dir.pwd, DEFAULT_FILENAME)
+      load DEFAULT_FILENAME
     end
   end
 end
--- spec/lib/recipe_spec.rb
+++ spec/lib/recipe_spec.rb
@@ -32,4 +32,26 @@ describe Niman::Recipe do
       expect(Niman::Recipe.configuration.instructions.length).to eq 0
     end
   end
+
+  describe '.from_file' do
+    before do
+      content = <<-EOS
+      Niman::Recipe.configure do |config|
+        config.file do |file|
+          file.path = '/home/bob/hello.txt'
+          file.content = 'hello from alice'
+        end
+      end
+      EOS
+      File.open(Niman::Recipe::DEFAULT_FILENAME, "w") {|handle| handle.write(content) }
+    end
+    after do
+      File.delete(Niman::Recipe::DEFAULT_FILENAME)
+    end
+
+    it 'loads "Nimanfile" by default' do
+      Niman::Recipe.from_file
+      expect(Niman::Recipe.configuration.instructions.length).to eq 1
+    end
+  end
 end
