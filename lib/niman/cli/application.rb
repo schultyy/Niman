d39b78773cf11a1e6e57a031e82dff7817487120
Jan Schulte <hello@unexpected-co.de>
2015-02-07 00:39:19 +0100

    Can create new Nimanfile

--- lib/niman/cli/application.rb
+++ lib/niman/cli/application.rb
@@ -10,6 +10,16 @@ module Niman
       rescue LoadError => e
         error e.message
       end
+
+      desc "setup", "Generates an empty Nimanfile"
+      def setup
+        content = <<-EOS
+  Niman::Recipe.configure do |config|
+  end
+        EOS
+        File.open(Niman::Recipe::DEFAULT_FILENAME, "w") { |handle| handle.write(content) }
+        say "Created new file #{Niman::Recipe::DEFAULT_FILENAME}"
+      end
     end
   end
 end
