5c789247eafe477319b7bc35d72e38d41a98609a
Jan Schulte <hello@unexpected-co.de>
2015-02-07 23:11:39 +0100

    Installer uses sudo for installing packages

--- lib/niman/installer.rb
+++ lib/niman/installer.rb
@@ -9,9 +9,9 @@ module Niman
     attribute :shell, Object
 
     def install(packages)
-      package_manager = managers.fetch(shell.os) { raise Niman::InstallError }
+      package_manager = managers.fetch(shell.os.to_sym) { raise Niman::InstallError, shell.os }
       Array(packages).each do |package|
-        shell.exec("#{package_manager} install #{package.name}")
+        shell.exec("#{package_manager} install #{package.name}", true)
       end
     end
   end
--- spec/lib/installer_spec.rb
+++ spec/lib/installer_spec.rb
@@ -29,8 +29,8 @@ describe Niman::Installer do
   describe "#install" do
     let(:shell) { double() }
     subject(:installer) { Niman::Installer.new(managers: {
-        ubuntu: 'apt-get',
-        centos: 'yum'
+        debian: 'apt-get',
+        redhat: 'yum'
     }, shell: shell)}
     let(:vim_package) { Niman::Library::Package.new(name: 'vim') }
     let(:ssh_package) { Niman::Library::Package.new(name: 'ssh') }
@@ -38,14 +38,14 @@ describe Niman::Installer do
 
     context 'with valid operating system' do
       before do
-        allow(shell).to receive(:os).and_return(:ubuntu)
-        allow(shell).to receive(:exec)#
+        allow(shell).to receive(:os).and_return(:debian)
+        allow(shell).to receive(:exec)
         installer.install(packages)
       end
 
       ['vim', 'ssh'].each do |package|
         it "calls shell for #{package}" do
-          expect(shell).to have_received(:exec).with("apt-get install #{package}")
+          expect(shell).to have_received(:exec).with("apt-get install #{package}", true)
         end
       end
     end
