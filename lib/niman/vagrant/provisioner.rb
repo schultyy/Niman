a86caae4884b69c30bd11cbb9ddce22a19d98b62
Jan Schulte <hello@unexpected-co.de>
2015-02-08 14:42:18 +0100

    Add boilerplate code from Vagrant shell provisioner

--- lib/niman/vagrant/provisioner.rb
+++ lib/niman/vagrant/provisioner.rb
@@ -1,14 +1,44 @@
+require "vagrant/util/retryable"
 module VagrantPlugins
   class Provisioner < Vagrant.plugin("2", :provisioner)
+    include Vagrant::Util::Retryable
     def configure(root_config)
-      puts "CONFIGURE"
     end
 
     def provision
-      puts "PROVISION"
-      require 'niman'
-      app = Niman::CLI::Application.new
-      app.apply
+      @machine.communicate.tap do |comm|
+        info = nil
+        retryable(on: Vagrant::Errors::SSHNotReady, tries: 3, sleep: 2) do
+          info = @machine.ssh_info
+          raise Vagrant::Errors::SSHNotReady if info.nil?
+        end
+        user = info[:username]
+        # Execute it with sudo
+        comm.execute(
+          "uname -a",
+          sudo: config.privileged,
+          error_key: :ssh_bad_exit_status_muted
+        ) do |type, data|
+          handle_comm(type, data)
+        end
+      end
+
+      #require 'niman'
+      #app = Niman::CLI::Application.new
+      #app.apply
+    end
+
+    def handle_comm(type, data)
+      if [:stderr, :stdout].include?(type)
+        #Output the data with the proper color based on the stream.
+        color = type == :stdout ? :green : :red
+        # Clear out the newline since we add one
+        data = data.chomp
+        return if data.empty?
+        options = {}
+        options[:color] = color if !config.keep_color
+        @machine.ui.info(data.chomp, options)
+      end
     end
   end
 end
