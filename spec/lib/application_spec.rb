e4a861a8e0e49e2c0256af5c2d528a878685ac18
Jan Schulte <hello@unexpected-co.de>
2015-02-15 23:10:49 +0100

    Add integration test for create file

--- spec/lib/application_spec.rb
+++ spec/lib/application_spec.rb
@@ -15,6 +15,28 @@ describe Niman::CLI::Application do
 
   after do
     Niman::Recipe.reset
+    File.delete(Niman::Recipe::DEFAULT_FILENAME)
+  end
+
+  describe "create files" do
+    before do
+      nimanfile = <<-EOS
+      #-*- mode: ruby -*-
+      # vi: set ft=ruby :
+      Niman::Recipe.configure do |config|
+        config.file "/etc/nginx/nginx.conf" do |file|
+          file.content = '...'
+        end
+      end
+      EOS
+      File.open(Niman::Recipe::DEFAULT_FILENAME, "w") {|h| h.write(nimanfile)}
+      allow(shell).to receive(:create_file)
+      application.apply
+    end
+
+    it 'creates a file with content' do
+      expect(shell).to have_received(:create_file).with('/etc/nginx/nginx.conf', '...')
+    end
   end
 
   describe "install packages" do
