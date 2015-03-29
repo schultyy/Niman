2d9342f2c626162694be16ac37d93bce42e4fc88
Jan Schulte <hello@unexpected-co.de>
2015-03-29 17:31:13 +0200

    Move install code to Package manager

--- lib/niman/cli/application.rb
+++ lib/niman/cli/application.rb
@@ -25,7 +25,7 @@ module Niman
           Niman::Recipe.from_file(file)
           config = Niman::Recipe.configuration
           installer = Niman::Installer.new(shell: client_shell,
-                                           managers: load_package_managers)
+                                           package_manager: load_package_manager)
           resolver    = Niman::PackageResolver.new(config.instructions)
           filehandler = Niman::FileHandler.new(client_shell)
           provisioner = Niman::Provisioner.new(installer, filehandler, @client_shell, resolver.resolve)
@@ -52,16 +52,17 @@ end
         say "Created new file #{filename}"
       end
       private
-      def load_package_managers
+      def load_package_manager
         managers = {}
         managers[:debian] = Niman::PackageManager.new(
          :os => :debian,
-         :install_command => 'apt-get -y install')
+         :install_command => 'apt-get -y install',
+         :shell => @client_shell)
         managers[:redhat] = Niman::PackageManager.new(
           :os => :redhat,
-          :install_command => 'yum -y install'
-        )
-        managers
+          :install_command => 'yum -y install',
+          :shell => @client_shell)
+        managers.fetch(@client_shell.os.to_sym) { raise Niman::InstallError, @client_shell.os }
       end
     end
   end
--- lib/niman/package_manager.rb
+++ lib/niman/package_manager.rb
@@ -1,9 +1,15 @@
 require 'virtus'
+require 'niman/shell'
 
 module Niman
   class PackageManager
     include Virtus.model
-    attribute :os, String, default: ''
+    attribute :shell, Niman::Shell
     attribute :install_command, String, default: ''
+    attribute :search_command, String, default: ''
+
+    def install(package_name)
+      shell.exec("#{install_command} #{package_name}",true)
+    end
   end
 end
